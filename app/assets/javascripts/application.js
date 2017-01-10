a0ef07c6ff26773fdface272cc9163b842168f10
vagrant <vagrant@centos-6-for-rails.(none)>
2017-01-10 10:23:46 +0900

    20170110

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,3 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require uikit/uikit
-//= require_tree .
---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+;( function ( document, window, index )
+{
+    var inputs = document.querySelectorAll( 'input.inputfile' );
+    console.log(inputs);
+    Array.prototype.forEach.call( inputs, function( input )
+    {
+        var label	 = input.nextElementSibling,
+            labelVal = label.innerHTML;
+
+        input.addEventListener( 'change', function( e )
+        {
+            var fileName = '';
+            fileName = e.target.value.split( '\\' ).pop();
+
+            if( fileName )
+                label.querySelector( 'span' ).innerHTML = fileName;
+            else
+                label.innerHTML = labelVal;
+        });
+
+        // Firefox bug fix
+        input.addEventListener( 'focus', function(){ input.classList.add( 'has-focus' ); });
+        input.addEventListener( 'blur', function(){ input.classList.remove( 'has-focus' ); });
+    });
+}( document, window, 0 ));
