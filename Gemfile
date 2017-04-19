5c756ee6ede098716db6f35e1c1601a2f749c2f1
etsracas <etsryacas@gmail.com>
2017-04-19 22:42:04 +0900

    from mysql to pgsql

--- Gemfile
+++ Gemfile
@@ -1,11 +1,11 @@
 source 'https://rubygems.org'
 
-ruby '2.2.2'
+ruby '2.3.4'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.7.1'
 # Use mysql as the database for Active Record
-gem 'mysql2', '>= 0.3.13', '< 0.5'
+gem 'pg'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- db/schema.rb
+++ db/schema.rb
@@ -13,45 +13,48 @@
 
 ActiveRecord::Schema.define(version: 20161219134403) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "yamalog_pics", force: :cascade do |t|
-    t.text     "image",      limit: 65535
-    t.datetime "created_at",                               null: false
-    t.datetime "updated_at",                               null: false
-    t.integer  "yamalog_id", limit: 4
-    t.boolean  "deletepic",                default: false
+    t.text     "image"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.integer  "yamalog_id"
+    t.boolean  "deletepic",  default: false
   end
 
   add_index "yamalog_pics", ["yamalog_id"], name: "index_yamalog_pics_on_yamalog_id", using: :btree
 
   create_table "yamalogs", force: :cascade do |t|
-    t.integer  "user_id",     limit: 4
-    t.date     "yama_date",                 null: false
-    t.string   "yama_name",   limit: 255,   null: false
-    t.string   "weather",     limit: 255,   null: false
-    t.string   "member",      limit: 255,   null: false
-    t.text     "route",       limit: 65535, null: false
-    t.text     "gpslog",      limit: 65535
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.text     "yamalog_pic", limit: 65535
+    t.integer  "user_id"
+    t.date     "yama_date",   null: false
+    t.string   "yama_name",   null: false
+    t.string   "weather",     null: false
+    t.string   "member",      null: false
+    t.text     "route",       null: false
+    t.text     "gpslog"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "yamalog_pic"
   end
 
   add_index "yamalogs", ["user_id", "yama_name"], name: "index_yamalogs_on_user_id_and_yama_name", using: :btree
